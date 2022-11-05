6788e75554665e5205f9e9b5222b3b7144da932f
Adrian <adrianvalverde45@gmail.com>
2022-11-05 16:51:30 -0400

    Initial commit to setup project.

--- project.js
+++ project.js
@@ -1,1 +1,12 @@
-console.log("yes this works!")
+// project code
+function callBackFunction(){
+    const sayHi = console.log(`Hello! I am a callback function! Since the main function was invoked I got invoked as well!`)
+    return sayHi
+}
+
+function mainFunction(callBackFunction){
+    console.log(`Hello! I am the main function and I just got invoked!`)
+    callBackFunction()
+}
+
+mainFunction(callBackFunction)
